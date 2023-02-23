82aa1c788f7d0fef83a6379308eab69f76ae3cae
alexey <63682890+Bulmenisaurus@users.noreply.github.com>
2023-02-23 14:19:21 -0800

    moving of pieces

--- src/board.ts
+++ src/board.ts
@@ -13,8 +13,6 @@ export class Board {
     board: Piece[];
     constructor() {
         this.board = Array<Piece>(8 * 8).fill(PIECE_NONE);
-
-        console.log(this.board);
     }
 
     getPiece(x: number, y: number): Piece {
@@ -42,33 +40,108 @@ export class Board {
     }
 }
 
-export const renderBoard = (board: Board): HTMLDivElement => {
-    const boardContainer = document.createElement('div');
-    boardContainer.id = 'board-container';
+export class InteractiveBoard {
+    board: Board;
+    boardElement: HTMLElement;
+    boardTileContainers: HTMLDivElement[];
+    selectedTileCoordinates: undefined | [number, number];
+    constructor(boardElement: HTMLElement) {
+        this.board = new Board();
 
-    const tileContainers: HTMLDivElement[] = board.coordinates().map(([x, y]) => {
-        const tileContainer = document.createElement('div');
-        tileContainer.classList.add('tile');
-        tileContainer.classList.add(board.getTileColor(x, y) === TILE_BLACK ? 'black' : 'white');
+        this.boardElement = boardElement;
+        this.boardTileContainers = this._initializeTileElements();
 
-        const tilePiece = board.getPiece(x, y);
+        this.selectedTileCoordinates = undefined;
 
-        switch (tilePiece) {
-            case PIECE_NONE: {
-                break;
+        boardElement.addEventListener('click', (ev) => {
+            this.onClick(ev);
+        });
+    }
+
+    _initializeTileElements() {
+        const tileContainers: HTMLDivElement[] = this.board.coordinates().map(([x, y]) => {
+            const tileContainer = document.createElement('div');
+            tileContainer.classList.add('tile');
+            tileContainer.classList.add(this.getTileColor(x, y) === TILE_BLACK ? 'black' : 'white');
+            tileContainer.dataset.selected = 'false';
+
+            const tilePiece = this.board.getPiece(x, y);
+
+            const pieceElement = document.createElement('div');
+            pieceElement.classList.add('piece');
+            pieceElement.dataset.pieceType = tilePiece;
+            tileContainer.appendChild(pieceElement);
+
+            return tileContainer;
+        });
+
+        return tileContainers;
+    }
+
+    getTileElement(x: number, y: number): HTMLDivElement {
+        return this.boardTileContainers[x + y * 8];
+    }
+
+    setPiece(x: number, y: number, piece: Piece) {
+        this.board.setPiece(x, y, piece);
+
+        const tileElement = this.getTileElement(x, y);
+        const pieceElement = tileElement.children[0] as HTMLElement;
+        pieceElement.dataset.pieceType = piece;
+    }
+
+    select(x: number, y: number) {
+        const tileElement = this.getTileElement(x, y);
+        tileElement.dataset.selected = 'true';
+    }
+
+    unselect(x: number, y: number) {
+        const tileElement = this.getTileElement(x, y);
+        tileElement.dataset.selected = 'false';
+    }
+
+    getTileColor(x: number, y: number): Tile {
+        return [TILE_WHITE as Tile, TILE_BLACK as Tile][(x + y) % 2];
+    }
+
+    onClick(event: MouseEvent) {
+        const mouseX = event.offsetX;
+        const mouseY = event.offsetY;
+
+        const boardFractionX = mouseX / this.boardElement.getBoundingClientRect().width;
+        const boardFractionY = mouseY / this.boardElement.getBoundingClientRect().height;
+
+        const boardTileX = Math.floor(boardFractionX * 8);
+        const boardTileY = Math.floor(boardFractionY * 8);
+
+        this.onTileClick(boardTileX, boardTileY);
+    }
+
+    onTileClick(tileX: number, tileY: number) {
+        const prevSelectedCoords = this.selectedTileCoordinates;
+
+        if (prevSelectedCoords !== undefined) {
+            this.unselect(prevSelectedCoords[0], prevSelectedCoords[1]);
+            this.selectedTileCoordinates = undefined;
+
+            if (prevSelectedCoords[0] === tileX && prevSelectedCoords[1] == tileY) {
+                return;
             }
 
-            default: {
-                const pieceElement = document.createElement('div');
-                pieceElement.classList.add('piece');
-                pieceElement.classList.add(tilePiece === PIECE_BLACK ? 'black' : 'white');
-                tileContainer.appendChild(pieceElement);
-            }
+            this.move(prevSelectedCoords[0], prevSelectedCoords[1], tileX, tileY);
+        } else {
+            this.selectedTileCoordinates = [tileX, tileY];
+            this.select(tileX, tileY);
         }
+    }
 
-        return tileContainer;
-    });
-    boardContainer.append(...tileContainers);
+    move(startX: number, startY: number, endX: number, endY: number) {
+        const pieceToMove = this.board.getPiece(startX, startY);
+        this.setPiece(startX, startY, PIECE_NONE);
+        this.setPiece(endX, endY, pieceToMove);
+    }
+}
 
-    return boardContainer;
+export const renderBoard = (board: InteractiveBoard, boardContainer: HTMLDivElement) => {
+    boardContainer.append(...board.boardTileContainers);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
 import * as Board from './board';
 
 const main = async (mainElement: HTMLElement) => {
-    const board = new Board.Board();
+    const boardElement = document.createElement('div');
+    boardElement.id = 'board-container';
 
+    mainElement.appendChild(boardElement);
+    const board = new Board.InteractiveBoard(boardElement);
+
+    board.setPiece(0, 0, Board.PIECE_WHITE);
+    board.setPiece(0, 1, Board.PIECE_WHITE);
     board.setPiece(1, 0, Board.PIECE_WHITE);
+    board.setPiece(2, 0, Board.PIECE_WHITE);
+    board.setPiece(1, 1, Board.PIECE_WHITE);
+    board.setPiece(0, 2, Board.PIECE_WHITE);
+    board.setPiece(3, 0, Board.PIECE_WHITE);
+    board.setPiece(2, 1, Board.PIECE_WHITE);
+    board.setPiece(1, 2, Board.PIECE_WHITE);
+    board.setPiece(0, 3, Board.PIECE_WHITE);
 
-    mainElement.appendChild(Board.renderBoard(board));
+    board.setPiece(7, 7, Board.PIECE_BLACK);
+    board.setPiece(6, 7, Board.PIECE_BLACK);
+    board.setPiece(7, 6, Board.PIECE_BLACK);
+    board.setPiece(5, 7, Board.PIECE_BLACK);
+    board.setPiece(6, 6, Board.PIECE_BLACK);
+    board.setPiece(7, 5, Board.PIECE_BLACK);
+    board.setPiece(7, 4, Board.PIECE_BLACK);
+    board.setPiece(6, 5, Board.PIECE_BLACK);
+    board.setPiece(5, 6, Board.PIECE_BLACK);
+    board.setPiece(4, 7, Board.PIECE_BLACK);
+
+    Board.renderBoard(board, boardElement);
 };
 
 window.addEventListener('load', () => {
