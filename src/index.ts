e352394f8ddc89ef77a7d95547b409ed9197b357
alexey <63682890+Bulmenisaurus@users.noreply.github.com>
2023-02-24 17:52:15 -0800

    change position of pieces

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,27 @@ const main = async (mainElement: HTMLElement) => {
     mainElement.appendChild(boardElement);
     const board = new Board.InteractiveBoard(boardElement);
 
-    board.setPiece(0, 0, Board.PIECE_WHITE);
-    board.setPiece(0, 1, Board.PIECE_WHITE);
-    board.setPiece(1, 0, Board.PIECE_WHITE);
-    board.setPiece(2, 0, Board.PIECE_WHITE);
-    board.setPiece(1, 1, Board.PIECE_WHITE);
-    board.setPiece(0, 2, Board.PIECE_WHITE);
-    board.setPiece(3, 0, Board.PIECE_WHITE);
-    board.setPiece(2, 1, Board.PIECE_WHITE);
-    board.setPiece(1, 2, Board.PIECE_WHITE);
-    board.setPiece(0, 3, Board.PIECE_WHITE);
+    board.setPiece(0, 7, Board.PIECE_WHITE);
+    board.setPiece(0, 6, Board.PIECE_WHITE);
+    board.setPiece(1, 7, Board.PIECE_WHITE);
+    board.setPiece(2, 7, Board.PIECE_WHITE);
+    board.setPiece(1, 6, Board.PIECE_WHITE);
+    board.setPiece(0, 5, Board.PIECE_WHITE);
+    board.setPiece(3, 7, Board.PIECE_WHITE);
+    board.setPiece(2, 6, Board.PIECE_WHITE);
+    board.setPiece(1, 5, Board.PIECE_WHITE);
+    board.setPiece(0, 4, Board.PIECE_WHITE);
 
-    board.setPiece(7, 7, Board.PIECE_BLACK);
-    board.setPiece(6, 7, Board.PIECE_BLACK);
-    board.setPiece(7, 6, Board.PIECE_BLACK);
-    board.setPiece(5, 7, Board.PIECE_BLACK);
-    board.setPiece(6, 6, Board.PIECE_BLACK);
-    board.setPiece(7, 5, Board.PIECE_BLACK);
-    board.setPiece(7, 4, Board.PIECE_BLACK);
-    board.setPiece(6, 5, Board.PIECE_BLACK);
-    board.setPiece(5, 6, Board.PIECE_BLACK);
-    board.setPiece(4, 7, Board.PIECE_BLACK);
+    board.setPiece(7, 0, Board.PIECE_BLACK);
+    board.setPiece(6, 0, Board.PIECE_BLACK);
+    board.setPiece(7, 1, Board.PIECE_BLACK);
+    board.setPiece(5, 0, Board.PIECE_BLACK);
+    board.setPiece(6, 1, Board.PIECE_BLACK);
+    board.setPiece(7, 2, Board.PIECE_BLACK);
+    board.setPiece(7, 3, Board.PIECE_BLACK);
+    board.setPiece(6, 2, Board.PIECE_BLACK);
+    board.setPiece(5, 1, Board.PIECE_BLACK);
+    board.setPiece(4, 0, Board.PIECE_BLACK);
 
     Board.renderBoard(board, boardElement);
 };
